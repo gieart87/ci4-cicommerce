bc4d8f153b4947347c9f5fee24f3268263546bd2
sugiarto <sugiarto@gie-art.com>
2020-07-15 03:25:21 +0700

    Installed CodeIgniter 4 and added user authentication with IonAuth

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,11 @@
+<?php namespace App\Controllers;
+
+class Auth extends \IonAuth\Controllers\Auth
+{
+    /**
+     * If you want to customize the views,
+     *  - copy the ion-auth/Views/auth folder to your Views folder,
+     *  - remove comment
+     */
+    protected $viewsFolder = 'auth';
+}

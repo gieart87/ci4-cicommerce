ae13848219f6cefa06df1ac329dd00d9e22ae855
sugiarto <sugiarto@gie-art.com>
2020-08-26 00:05:05 +0700

    added: brands management

---
+++ app/Models/BrandModel.php
@@ -0,0 +1,43 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BrandModel extends Model
+{
+    protected $table      = 'brands';
+	protected $primaryKey = 'id';
+
+	protected $returnType     = 'App\Entities\Brand';
+
+	protected $allowedFields = ['name', 'slug'];
+
+	protected $useTimestamps = false;
+	protected $createdField  = 'created_at';
+	protected $updatedField  = 'updated_at';
+
+	protected $validationRules    = [
+		'name'	=> 'required|min_length[3]',
+	];
+
+	protected $validationMessages = [];
+	protected $skipValidation     = false;
+
+	protected $beforeInsert = ['generateSlug'];
+
+	protected function generateSlug(array $data)
+	{
+		$slug = strtolower(url_title($data['data']['name']));
+		$name = trim($data['data']['name']);
+
+		$category = $this->where('name', $name)->orderBy('id', 'DESC')->first();
+		if ($category) {
+			$slugs = explode('-', $category->slug);
+			$slugNumber = !(empty($slugs[1])) ? ((int)$slugs[1] + 1) : 1;
+			$slug = $slug. '-' .$slugNumber;
+		}
+
+		$data['data']['slug'] = $slug;
+
+		return $data;
+	}
+}
